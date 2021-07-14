e34db9074245f3247968f489272a1e3022db5d9d
Holy <k0k05sal12@gmail.com>
2021-07-14 00:02:55 -0400

    correccion Error
    xd

--- mapa.py
+++ mapa.py
@@ -10,6 +10,7 @@ coleccion1 = baseDatos['archivos']
 
 mapa = folium.Map(location =[-39.823641, -73.226158], zoom_start = 14)
 
+for doc in coleccion1.find():
     latitud = doc['latitud']
     longitud = doc['longitud']
     fecha = doc['fecha_de_grabacion']
